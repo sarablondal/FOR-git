2cbd0ba029aa401be7e9671bcf852f727df39b6e
Sara Blöndal <sarablondal@gmail.com>
2019-09-05 14:29:26 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,10 @@
+# skref 1. setja inn hversu margar tölur eiga að prentast
+# skref 2. búa til kassa sem geymir síðustu þrjár tölurnar 
+# og nýjustu töluna sem á að prenta, nefna kassa eftir innihaldi
+# skref 3. gera for loop sem tekur inn upplysingar ur skrefi 1 
+# skref 4. ef tala nr. 1 er ekki með gildi gefa fyrstu þremur gildunum
+# gildin 1, 2 og 3.
+# skref 4. ef fyrsta talan er stærri en 0 þá prenta út summu síðustu 3 talnanna.
+# skref 5. Prenta summu síðustu 3 talna.
+# skref 6. Tala 1 tekur gildi tölu 2, tala 2 tekur gildi tölu 3 og tala 3 tekur gildi tölu fjögur.
+# skref 7. endurtaka skref 5 n sinnum
