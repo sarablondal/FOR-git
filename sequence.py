0efd1160c6934629f3c366af78edb3ae038b865e
Sara Blöndal <sarablondal@gmail.com>
2019-09-05 14:39:55 +0000

    Added code

--- sequence.py
+++ sequence.py
@@ -21,3 +21,9 @@ for i in range(1,n):
         print(num1)
         print(num2)
         print(num3)
+    else:
+        num4= num1+num2+num3
+        print(num4)
+        num1 = num2
+        num2 = num3
+        num3 = num4
