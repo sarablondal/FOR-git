d30a2b342e0b505d4059e8f0a92019dea480e8e3
Sara Blöndal <sarablondal@gmail.com>
2019-09-05 14:36:34 +0000

    Added for loop and first 3 numbers

--- sequence.py
+++ sequence.py
@@ -8,3 +8,16 @@
 # skref 5. Prenta summu síðustu 3 talna.
 # skref 6. Tala 1 tekur gildi tölu 2, tala 2 tekur gildi tölu 3 og tala 3 tekur gildi tölu fjögur.
 # skref 7. endurtaka skref 5 n sinnum
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+num1 = 0
+num2 = 0
+num3 = 0
+num4 = 0
+for i in range(1,n):
+    if num1 == 0:
+        num1 = 1
+        num2 = 2
+        num3 = 3
+        print(num1)
+        print(num2)
+        print(num3)
