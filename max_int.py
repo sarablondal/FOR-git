2adfd4f48de293a087716a4cff934b8500268a63
Sara Blöndal <sarablondal@gmail.com>
2019-09-05 14:03:24 +0000

    Updated code

--- max_int.py
+++ max_int.py
@@ -3,3 +3,12 @@
 # 3. Ef innsett tala er stærri en vistuð stærsta tala þá verður innsett tala að stærstu.
 # 4. skref while number =>0 þá endurtaka skref 1 og skref 3
 # 5. skref ef innsett tala er minni en 0 prentast stærsta vistaða tala.
+max_int = 0
+num_int = int(input("Input a number: "))    # Do not change this line
+if num_int >= 0:
+    max_int = num_int
+    while num_int >= 0:
+        num_int = int(input("Input a number: "))
+        if num_int > max_int:
+            max_int = num_int
+print("The maximum is", max_int)    # Do not change this line
