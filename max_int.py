0f2e58a49893d4b4af18d25b9dd5c28c5de2cfff
Sara Blöndal <sarablondal@gmail.com>
2019-09-05 13:52:04 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,5 @@
+# 1. skref setja inn tölu.
+# 2. skref Búa til if setningu þar sem innsett tala þarf að vera 0<= annars gerist ekkert.
+# 3. Ef innsett tala er stærri en vistuð stærsta tala þá verður innsett tala að stærstu.
+# 4. skref while number =>0 þá endurtaka skref 1 og skref 3
+# 5. skref ef innsett tala er minni en 0 prentast stærsta vistaða tala.
